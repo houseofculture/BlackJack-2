123288ace1ad88f5699dc0e38e1af8f0beb80aed
houseofculture <mortypines@gmail.com>
2016-11-25 19:16:37 +0300

    25.11.2016

---
+++ src/com/company/Intellect.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by student2 on 18.11.16.
+ */
+public abstract class Intellect {
+    public  abstract Command decide(int score);
+}
---
+++ src/com/company/LimitIntellect.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * Created by student2 on 18.11.16.
+ */
+public class LimitIntellect extends Intellect {
+    public LimitIntellect(int limit) {
+        this.limit = limit;
+    }
+    private int limit;
+
+    public Command decide(int score){
+        if(score<limit)
+            return Command.HIT;
+        else
+            return Command.STAND;
+    }
+}
